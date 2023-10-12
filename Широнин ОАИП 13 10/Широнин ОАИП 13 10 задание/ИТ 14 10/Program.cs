45c4d0cba0c2ec9f8b19bd5d52e9b6aaa58cd0d4
Max173Shironin <115072755+Max173Shironin@users.noreply.github.com>
2023-10-12 20:34:39 +0300

    Add files via upload

---
+++ Широнин ОАИП 13 10/Широнин ОАИП 13 10 задание/ИТ 14 10/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Широнин Максим ИСП.22А ИТ 14.10 Решение варианта задания 4");
+double a = 1.1;
+double a1 = 1.6;
+double Y = 0; double Y1 = 0;
+for (double x = 0; x <= 5; x += 0.5)
+{
+    Y = (a * x + Math.Log(1.0 + a * x)) / (1.0 + Math.Pow(a * x, 2));
+    Y1 = (a1 * x + Math.Log(1.0 + a1 * x)) / (1.0 + Math.Pow(a1 * x, 2));
+    Console.WriteLine($"При a = 1.1, 1.6 и x = {x} ;\n " +
+    $"{a} * {x} + {Math.Round(Math.Log(1 + a1 * x),2)}/{1.0} + {Math.Round(Math.Pow((a * x), 2), 2)} = {Math.Round(Y, 2)} \n " +
+    $"{a1} * {x} + {Math.Round(Math.Log(1 + a1 * x), 2)}/{1.0} + {Math.Round(Math.Pow((a1 * x), 2), 2)} = {Math.Round(Y1, 2)}");
+}
+Console.ReadLine();
